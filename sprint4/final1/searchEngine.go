cf927ebd466e820d87cc2bc5a25b1c00feb238fe
Roman Agaltsev <agar@rarus.ru>
2024-05-02 11:24:02 +0300

    Спринт 4. Задача Финал A.

--- sprint4/final1/searchEngine.go
+++ sprint4/final1/searchEngine.go
@@ -2,79 +2,89 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"cmp"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
-const (
-	A = 997
-	M = 1000000007
-)
+const docsLimit = 5
 
-func getHash(s string) int {
-	hash := 0
-	for i := 0; i < len(s); i++ {
-		hash = (hash*A%M + int(s[i])) % M
-	}
-	return hash
-}
-
-func getWordsCount(line string) map[int]int {
-	wordsCount := make(map[int]int)
-	for _, word := range strings.Split(line, " ") {
-		wordsCount[getHash(word)]++
-	}
-	return wordsCount
-}
-
-func getUniqueHashes(query string) []int {
-	hashes := make(map[int]int)
-	for _, word := range strings.Split(query, " ") {
-		hash := getHash(word)
-		if _, ok := hashes[hash]; !ok {
-			hashes[hash]++
+func getSearchIndex(scanner *bufio.Scanner, n int) map[string]map[int]int {
+	index := make(map[string]map[int]int)
+	for i := 0; i < n; i++ {
+		for _, word := range strings.Split(readLine(scanner), " ") {
+			if wordsCount, ok := index[word]; ok {
+				wordsCount[i+1]++
+			} else {
+				index[word] = map[int]int{i + 1: 1}
+			}
 		}
 	}
-	result := make([]int, 0)
-	for hash := range hashes {
-		result = append(result, hash)
+	return index
+}
+
+func getUniqueWords(query string) []string {
+	wordsCount := make(map[string]int)
+	for _, word := range strings.Split(query, " ") {
+		wordsCount[word]++
+	}
+	result := make([]string, 0)
+	for word := range wordsCount {
+		result = append(result, word)
 	}
 	return result
 }
 
-func getQueryRelevance(index []map[int]int, query string) map[int]int {
-	result := make(map[int]int)
-	queryHashes := getUniqueHashes(query)
-	for _, queryHash := range queryHashes {
-		for i, document := range index {
-			result[i] += document[queryHash]
+func getQueryRelevance(index map[string]map[int]int, query string) []int {
+	relevance := make(map[int]int)
+	words := getUniqueWords(query)
+	for _, word := range words {
+		if docs, ok := index[word]; ok {
+			for doc, count := range docs {
+				relevance[doc] += count
+			}
 		}
+	}
+	relevanceSorted := make([][2]int, 0)
+	for doc, rel := range relevance {
+		if rel != 0 {
+			relevanceSorted = append(relevanceSorted, [2]int{doc, rel})
+		}
+	}
+	slices.SortFunc(relevanceSorted, func(a, b [2]int) int {
+		if res := cmp.Compare(b[1], a[1]); res != 0 {
+			return res
+		}
+		if res := cmp.Compare(a[0], b[0]); res != 0 {
+			return res
+		}
+		return 0
+	})
+	result := make([]int, 0)
+	for i := 0; i < docsLimit && i < len(relevanceSorted); i++ {
+		result = append(result, relevanceSorted[i][0])
 	}
 	return result
 }
 
 func main() {
 	scanner := makeScanner()
-	
 	n := readInt(scanner)
-	index := make([]map[int]int, n)
-	for i := 0; i < n; i++ {
-		index[i] = getWordsCount(readLine(scanner))
-	}
-	
-	//fmt.Print(index)
-	
+	index := getSearchIndex(scanner, n)
 	m := readInt(scanner)
+	result := make([][]int, m)
 	for i := 0; i < m; i++ {
-		//printArray(getQueryRelevance(index, readLine(scanner)))
-		fmt.Print(getQueryRelevance(index, readLine(scanner)))
+		result[i] = getQueryRelevance(index, readLine(scanner))
+	}
+	for i := 0; i < m; i++ {
+		printArray(result[i])
 	}
 }
 
 func makeScanner() *bufio.Scanner {
-	const maxCapacity = 3 * 1024 * 1024
+	const maxCapacity = 10 * 1024 * 1024
 	buf := make([]byte, maxCapacity)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Buffer(buf, maxCapacity)
