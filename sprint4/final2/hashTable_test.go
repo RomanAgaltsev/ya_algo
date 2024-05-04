c74858d88b201449c2785dc09e03bf32775ae468
Roman Agaltsev <agar@rarus.ru>
2024-05-04 15:07:11 +0300

    Спринт 4. Задача Финал B.

--- sprint4/final2/hashTable_test.go
+++ sprint4/final2/hashTable_test.go
@@ -5,19 +5,19 @@ import "testing"
 func TestHashTable(t *testing.T) {
 	t.Run("1", func(t *testing.T) {
 		commands := []struct{
-			command Command
+			command сommand
 			result string
 		}{
-			{Command{"get", 1, 0}, "None"},
-			{Command{"put", 1, 10}, ""},
-			{Command{"put", 2, 4}, ""},
-			{Command{"get", 1, 0}, "10"},
-			{Command{"get", 2, 0}, "4"},
-			{Command{"delete", 2, 0}, "4"},
-			{Command{"get", 2, 0}, "None"},
-			{Command{"put", 1, 5}, ""},
-			{Command{"get", 1, 0}, "5"},
-			{Command{"delete", 2, 0}, "None"},
+			{сommand{"get", 1, 0}, "None"},
+			{сommand{"put", 1, 10}, ""},
+			{сommand{"put", 2, 4}, ""},
+			{сommand{"get", 1, 0}, "10"},
+			{сommand{"get", 2, 0}, "4"},
+			{сommand{"delete", 2, 0}, "4"},
+			{сommand{"get", 2, 0}, "None"},
+			{сommand{"put", 1, 5}, ""},
+			{сommand{"get", 1, 0}, "5"},
+			{сommand{"delete", 2, 0}, "None"},
 		}
 		hashTable := NewHashTable()
 		for _, command := range commands {
@@ -30,17 +30,17 @@ func TestHashTable(t *testing.T) {
 	
 	t.Run("2", func(t *testing.T) {
 		commands := []struct{
-			command Command
+			command сommand
 			result string
 		}{
-			{Command{"get", 9, 0}, "None"},
-			{Command{"delete", 9, 0}, "None"},
-			{Command{"put", 9, 1}, ""},
-			{Command{"get", 9, 0}, "1"},
-			{Command{"put", 9, 2}, ""},
-			{Command{"get", 9, 0}, "2"},
-			{Command{"put", 9, 3}, ""},
-			{Command{"get", 9, 0}, "3"},
+			{сommand{"get", 9, 0}, "None"},
+			{сommand{"delete", 9, 0}, "None"},
+			{сommand{"put", 9, 1}, ""},
+			{сommand{"get", 9, 0}, "1"},
+			{сommand{"put", 9, 2}, ""},
+			{сommand{"get", 9, 0}, "2"},
+			{сommand{"put", 9, 3}, ""},
+			{сommand{"get", 9, 0}, "3"},
 		}
 		hashTable := NewHashTable()
 		for _, command := range commands {
